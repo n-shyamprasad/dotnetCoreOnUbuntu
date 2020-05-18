a400255287a76e0946d4a0652e23ae656f0daa9e
n-shyamprasad <n.shyamprasad@hotmail.com>
2020-05-19 06:54:32 +0800

    my first commit

---
+++ fibogen-app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace fibogen_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var FibGenerator = new FibonacciGenerator();
+           foreach(var digit in  FibGenerator.Generate(15))
+           {
+               Console.WriteLine(digit);
+           }
+        }
+    }
+}
---
+++ fibonacci-app/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace fibonacci_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           if(args.Length > 0)
+           {
+                Console.WriteLine($"Hello {args[0]}!");
+           }
+           else
+           {
+            Console.WriteLine("Hello..");
+           }
+           Console.WriteLine("Fibonacci numbers from 1-15:");
+            for(int i=0; i<15; i++)
+            {
+            Console.WriteLine($"{i+1} : {FibonacciNumber(i)} ");
+            }
+        }
+
+        static int FibonacciNumber(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int temp;
+            for(int i=0; i<n; i++)
+            {
+                temp = a;
+                a = b;
+                b = b + temp;
+            } 
+
+            return a;               
+        }
+    }
+}
